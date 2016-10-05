e955633321a434735ff7d654773df27eaf30dae5
marcelestial <marcel@spaceside.com>
2016-10-05 13:51:18 -0700

    10/5/16

--- app/src/main/java/com/example/mal/dragonfinder/Profile1Activity.java
+++ app/src/main/java/com/example/mal/dragonfinder/Profile1Activity.java
@@ -2,9 +2,10 @@ package com.example.mal.dragonfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TabHost;
 
 public class Profile1Activity extends AppCompatActivity {
-
+    TabHost tabHost;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
